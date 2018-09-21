a011fa01f26d8fdd628211e4360d03d3d37d6a33
U-BSD\349973 <349973@SHS-10L74C3F1.beaverton.k12.or.us>
2018-09-21 09:03:41 -0700

    removes not letters, capitalizes

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -8,22 +8,34 @@ using namespace std;
 int main()
 {
   int i = 0;
+  int j = 0;
+  int len = 0;
   char c;
   char str [80];
   char str2 [80];
+  bool palindrome;
   cin.get(str, 80);
   
-  while (str[i] > 0) {
-    if (!(ispunct(str[i]))) {
-      str2[i] = toupper(str[i]);
-       i++;
-	}
-    else {
+  while (str[i] >= 0) {
+    if (!(ispunct(str[i])) && !(isspace(str[i]))) {
+      str2[j] = toupper(str[i]);
       i++;
-}
+      j++;
+    }
+      if (ispunct(str[i])) {
+      i++;
+    }
+   if (isspace(str[i])) {
+     i++;
+    }
   }
 
-  //strcpy (str2, str);
+  strcpy (str, str2);
+  len = strlen(str);
+  cout << len << endl;
+
+
+  
   cout << "String 1:";
   cout << str << endl;
   cout << "String 2:";
